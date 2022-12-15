a9b1de6684e96f2d0340b7285c0def7dd446e295
SwapnilVedpathak-sv <swapnilvedpathak.sv@gmail.com>
2022-12-15 20:21:21 +0530

    Port changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const dbURI = `mongodb+srv://codeimplants:Code2022@paymenttracker.5hffast.mongodb.net/payment-tracker`;
 
 const payment = require("./api/routes/payment");
