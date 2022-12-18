5bfc5c023669adc56631d6362dfe59d86a0e1ff3
SwapnilVedpathak-sv <swapnilvedpathak.sv@gmail.com>
2022-12-18 11:35:02 +0530

    added cross origin

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const dbURI = `mongodb+srv://codeimplants:Code2022@paymenttracker.5hffast.mongod
 
 const payment = require("./api/routes/payment");
 
+// app.use(express.static('public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
